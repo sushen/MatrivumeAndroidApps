b6f876b1539d80d07937cd1d0b74f0ed02cc44ea
sushen <sushenbiswasaga@gmail.com>
2019-10-30 22:03:53 +0600

    Firebase Signin Working

--- app/src/main/java/blog/video/biswas/matrivumeandroidapps/MainActivity.java
+++ app/src/main/java/blog/video/biswas/matrivumeandroidapps/MainActivity.java
@@ -4,11 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {public static final int RC_SIGN_IN = 1;
+
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        startActivityForResult(
+                // Get an instance of AuthUI based on the default app
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.EmailBuilder().build(),
+                                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                                new AuthUI.IdpConfig.GoogleBuilder().build()
+                        ))
+                        .build(),
+                RC_SIGN_IN);
     }
+
 }
